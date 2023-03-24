1cb28438c4e27303da42bdc1c384894fa90be16a
Lone Mortensen <lmordev@gmail.com>
2023-03-24 16:09:42 -0700

    Added animation to introduction title and text.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 /* ====================================================================
 Project:  Portfolio Website  
-Description:  For documention of personal web development projects. 
+Description:  For documention of personal web development projects 
 
 ===== *** =====
 
 This document contains JavaScript code that:
-
-- Automatically updates the copyright year in the footer of 
-the website.
-
+- Opens and closes the hamburger navigation menu in mobile view.
+- Manages the animation of the introduction title and text on page load.
 - Makes the scroll-to-top button interactive so that: 
-a) it is visible only when the user scrolls on the page, and 
-b) it scrolls to the top of the page when the user clicks the button.
+    a) it is visible only when the user scrolls on the page, and 
+    b) it scrolls to the top of the page when the user clicks the button.
+- Automatically updates the copyright year.
 ==================================================================== */
 
 /* -------- MOBILE NAVIGATION -------- */ 
@@ -24,7 +23,6 @@ const mobileNavMenu = document.querySelector(".mobile-nav");
 const mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
 
 // Toggles the mobile nav menu and the close menu button:
-
 const openMobileNav = () => {
   mobileNavIcon.classList.toggle("open");
   mobileNavMenu.classList.toggle("hide-mobile-nav-menu");
@@ -34,7 +32,6 @@ const openMobileNav = () => {
 mobileNavIcon.addEventListener("click", openMobileNav);
 
 // Closes the mobile nav menu when user clicks on the menu links:
-
 const closeMobileNav = (n) => {
   mobileNavIcon.classList.remove("open");
   mobileNavMenu.classList.add("hide-mobile-nav-menu");
@@ -44,15 +41,51 @@ const closeMobileNav = (n) => {
 mobileNavLinks.forEach(n => n.addEventListener("click", closeMobileNav));
  
 
-/* -------- UPDATE COPYRIGHT YEAR -------- */ 
+/* -------- INTRODUCTION ANIMATION -------- */
 
-// Automatically updates copyright year in the website footer:
-
-const copyrightYear = document.querySelector(".copyright-year");
-
-let date = new Date();
-let year = date.getFullYear();
-copyrightYear.innerHTML = `<p>Copyright ${year}</p>`;
+const INTRO = (function () {
+    //Introduction title:
+    const introductionTitle = document.querySelector(".reveal-introduction-title");
+    //Introduction sub-title:
+    const introductionText = document.querySelector(".reveal-introduction-text");
+    //Span that changes text styling in introduction sub-title:
+    const textStyle = document.querySelector(".text-style");
+    
+    //Reveals introduction title with typing effect when page has loaded:
+    const revealIntroTitle = () => {
+      introductionText.classList.remove("show-introduction-text");
+      introductionTitle.innerText = "";
+      const titleText = "Welcome"; 
+      let i = 0;
+      const typingEffect = setInterval (() => {
+        introductionTitle.innerText += titleText.charAt(i); 
+        i++;
+        if (i === titleText.length) {
+          clearInterval(typingEffect);
+          revealIntroText();
+        }
+      }, 85);
+    };
+  
+    //Reveals introduction text after title has finished typing: 
+    const revealIntroText = () => {
+      const showText = () => {
+        introductionText.classList.add("show-introduction-text");
+      };
+      setTimeout(showText, 1000);
+    };
+  
+    //Changes styling for text span in introduction text:  
+    const changeTextStyle = () => {
+      textStyle.classList.add("change-text-style");
+    };
+  
+    // Detects when web page has loaded:
+    window.addEventListener("load", revealIntroTitle);
+  
+    // Detects when introduction text animation is completed:
+    introductionText.addEventListener("animationend", changeTextStyle) 
+})();
 
 
 /* -------- SCROLL BUTTON -------- */
@@ -63,7 +96,6 @@ const scrollToTopButton = document.querySelector(".scroll-top-button");
 const rootElement = document.documentElement;
 
 //Shows or hides the scroll-to-top button:
-
 const handleScroll = () => {
     //Calculates max pixels that can be scrolled vertically:
     const totalScrollLength = rootElement.scrollHeight - rootElement.clientHeight;
@@ -76,7 +108,6 @@ const handleScroll = () => {
 }; 
 
 //Scrolls to the top of the page:
-
 const scrollToTop = () => {
     //Specifies the position to scroll to:
     rootElement.scrollTo({
@@ -91,3 +122,12 @@ document.addEventListener("scroll", handleScroll);
 //Detects when user clicks the scroll-to-top button:
 scrollToTopButton.addEventListener("click", scrollToTop);
 
+/* -------- UPDATE COPYRIGHT YEAR -------- */ 
+
+// Automatically updates copyright year in the website footer:
+
+const copyrightYear = document.querySelector(".copyright-year");
+
+let date = new Date();
+let year = date.getFullYear();
+copyrightYear.innerHTML = `<p>Copyright ${year}</p>`;
