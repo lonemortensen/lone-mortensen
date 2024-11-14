8a78b2233044f5af0ac393eeb6176b38c04e1acd
Lone Mortensen <lmordev@gmail.com>
2024-11-13 16:29:12 -0800

    Added comments to clarify About section functionality

--- js/about.js
+++ js/about.js
@@ -1,9 +1,15 @@
 /* ====================================================================
 Project:  Portfolio Website Version 3
 Description:  For documention of personal web development projects. 
-Module: About.
+Module: about.js
 ========================================================================= */
 
+/**
+ * Initiates animation of sliders and 'yellow dot' in About section.
+ * Selects target html elements for animation.
+ * Uses the Intersection Observer API to watch for intersection changes for 'slider' html target elements. 
+ * Runs callback function to initiate animations when targets intersect with the Viewport (root).   
+*/
 export const animateAboutSection = () => {
     // Target element: first About slider:
     const aboutSliderOne = document.querySelector(".about-slider-1");
@@ -16,29 +22,29 @@ export const animateAboutSection = () => {
      * Creates intersection observers and sets options for sliders.
      */
     const observerSliderOne = () => {
-    // Options object contains settings for the observer:
-    const options = {
-        root: null, // Root element set to viewport
-        rootMargin: "0px",
-        threshold: 1
-    };
-    //Creates new observer, passing callback function and observer options.
-    const observer = new IntersectionObserver(startAboutAnimation, options);
-    // Passes target element to observer:
-    observer.observe(aboutSliderOne);
+        // Options object contains settings for the observer:
+        const options = {
+            root: null, // Root element set to viewport
+            rootMargin: "0px",
+            threshold: 1
+        };
+        //Creates new observer, passing callback function and observer options.
+        const observer = new IntersectionObserver(startAboutAnimation, options);
+        // Passes target element to observer:
+        observer.observe(aboutSliderOne);
     };
 
     const observerSliderTwo = () => {
-    // Options object contains settings for the observer:*/
-    const options = {
-        root: null, // Root element set to viewport
-        rootMargin: "0px",
-        threshold: 0.3
-    };
-    // Creates new observer, passing callback and observer options.
-    const observer = new IntersectionObserver(startAboutAnimation, options);
-    // Passes target element to observer:
-    observer.observe(aboutSliderTwo);
+        // Options object contains settings for the observer:*/
+        const options = {
+            root: null, // Root element set to viewport
+            rootMargin: "0px",
+            threshold: 0.3
+        };
+        // Creates new observer, passing callback and observer options.
+        const observer = new IntersectionObserver(startAboutAnimation, options);
+        // Passes target element to observer:
+        observer.observe(aboutSliderTwo);
     };
 
     /**
@@ -48,35 +54,34 @@ export const animateAboutSection = () => {
      * @param observer The IntersectionObserver instance allows for interaction with the observer.
      */
     const startAboutAnimation = (entries, observer) => {
-    entries.forEach((entry) => {
-        console.log(entry);
-        if (entry.isIntersecting) {
-        if (entry.target.id === "slider-1") {
-            console.log("we're intersecting slide 1"); // Works
-            observer.disconnect();
-            entry.target.classList.remove("hide-about-content"); // Works 
-            entry.target.classList.add("about-slide-left"); // Works - "slider 1" slides in. 
-        } else if (entry.target.id === "slider-2") {
-            console.log("we're intersecting slide 2"); // Works
-            observer.disconnect();
-            entry.target.classList.remove("hide-about-content"); // Works 
-            entry.target.classList.add("about-slide-right"); // Works - "slider 2" slides in.
-            revealAboutCircle();
-        }
-        // revealAboutCircle();
-        }
-    });
+        entries.forEach((entry) => {
+            console.log(entry);
+            if (entry.isIntersecting) {
+                if (entry.target.id === "slider-1") {
+                    console.log("we're intersecting slide 1"); // Works
+                    observer.disconnect();
+                    entry.target.classList.remove("hide-about-content"); // Works 
+                    entry.target.classList.add("about-slide-left"); // Works - "slider 1" slides in. 
+                } else if (entry.target.id === "slider-2") {
+                    console.log("we're intersecting slide 2"); // Works
+                    observer.disconnect();
+                    entry.target.classList.remove("hide-about-content"); // Works 
+                    entry.target.classList.add("about-slide-right"); // Works - "slider 2" slides in.
+                    revealAboutCircle();
+                }
+            }
+        });
     };
 
     /**
      * Reveals circle after second slider is in place. 
      */
     const revealAboutCircle = () => {
-    const showCircle = () => {
-        aboutCircle.classList.remove("hide-about-content");
-        aboutCircle.classList.add("about-drop-top");
-    };
-    setTimeout(showCircle, 2500);
+        const showCircle = () => {
+            aboutCircle.classList.remove("hide-about-content");
+            aboutCircle.classList.add("about-drop-top");
+        };
+        setTimeout(showCircle, 2500);
     };
 
     observerSliderOne();
