3489e5f745f61dbb15b66420520b935f484ae0cc
Lone Mortensen <lmordev@gmail.com>
2024-11-14 14:22:41 -0800

    Added intro.js for JS introduction animation code

---
+++ js/intro.js
@@ -0,0 +1,50 @@
+/* ====================================================================
+Project:  Portfolio Website Version 3
+Description:  For documention of personal web development projects. 
+Module: intro.js
+========================================================================= */
+
+/**
+ * Reveals introduction title with animation.
+ * Reveals introduction text.
+ * Adds styling to introduction text.
+*/
+
+// Introduction title:
+const introductionTitle = document.querySelector(".reveal-introduction-title");
+// Introduction text:
+const introductionText = document.querySelector(".reveal-introduction-text");
+// Span that changes text styling in introduction text:
+const textStyle = document.querySelector(".text-style");
+ 
+// Reveals introduction title with typing effect when page has loaded:
+export const revealIntroTitle = () => {
+  introductionText.classList.remove("show-introduction-text");
+  let titleText = introductionTitle.textContent;
+  introductionTitle.textContent = "";
+  let i = 0;
+  const typingEffect = setInterval (() => {
+    introductionTitle.textContent += titleText.charAt(i); 
+    i++;
+    if (i === titleText.length) {
+      clearInterval(typingEffect);
+      revealIntroText();
+    }
+  }, 100);
+};
+
+// Reveals introduction text after title has finished typing: 
+const revealIntroText = () => {
+  const showText = () => {
+    introductionText.classList.add("show-introduction-text");
+  };
+  setTimeout(showText, 1000);
+};
+
+// Changes styling for text span in introduction text:  
+const changeTextStyle = () => {
+  textStyle.classList.add("change-text-style");
+};
+
+// Detects when introduction text animation is completed:
+introductionText.addEventListener("animationend", changeTextStyle) 
