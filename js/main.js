60a9dd4a0aa1a12011cceff6f73b56a28bf07c95
Lone Mortensen <lmordev@gmail.com>
2025-06-05 15:34:04 -0700

    Experimented with throttling delay for scroll to top button

--- js/main.js
+++ js/main.js
@@ -100,7 +100,7 @@ animateAboutSection();
  * @arg displayScrollButton - The function that shows or hides the scroll-to-top button.
  * @arg 200 - The wait time in milliseconds before the function is called again.
 */
-document.addEventListener("scroll", limitFunctionCalls(displayScrollButton, 200));
+document.addEventListener("scroll", limitFunctionCalls(displayScrollButton, 125));
 
 
 /* -------- UPDATE COPYRIGHT YEAR -------- */ 
